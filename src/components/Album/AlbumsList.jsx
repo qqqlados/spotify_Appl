ea8911d90f2fd10140836ddf0b7e32d6661224bd
wladus <wladusdrem@gmail.com>
2024-07-17 20:25:39 +0300

    deleted several unnecessary files

--- src/components/Album/AlbumsList.jsx
+++ src/components/Album/AlbumsList.jsx
@@ -1,31 +1,31 @@
-import { Link } from 'react-router-dom'
-import { useGetNewReleasesQuery } from '../../api/albums'
-import { useAlbumImagesArray } from '../../hooks/useAlbums'
-import styles from './Album.module.scss'
+// import { Link } from 'react-router-dom'
+// import { useGetNewReleasesQuery } from '../../api/albums'
+// import { useAlbumImagesArray } from '../../hooks/useAlbums'
+// import styles from './Album.module.scss'
 
-const AlbumsList = () => {
-	const { error, isLoading } = useGetNewReleasesQuery()
+// const AlbumsList = () => {
+// 	const { error, isLoading } = useGetNewReleasesQuery()
 
-	const { albums, images } = useAlbumImagesArray()
+// 	const { albums, images } = useAlbumImagesArray()
 
-	let content
-	if (isLoading) {
-		content = <div>Loading...</div>
-	} else if (error) {
-		content = error
-	} else {
-		content = (
-			<div className={styles.new_releases_container}>
-				{albums.map((item, index) => (
-					<Link key={item.id} to={`/new-releases/${item.id}`}>
-						<img key={index} src={images[index].url} alt='Something' />
-					</Link>
-				))}
-			</div>
-		)
-	}
+// 	let content
+// 	if (isLoading) {
+// 		content = <div>Loading...</div>
+// 	} else if (error) {
+// 		content = error
+// 	} else {
+// 		content = (
+// 			<div className={styles.new_releases_container}>
+// 				{albums.map((item, index) => (
+// 					<Link key={item.id} to={`/new-releases/${item.id}`}>
+// 						<img key={index} src={images[index].url} alt='Something' />
+// 					</Link>
+// 				))}
+// 			</div>
+// 		)
+// 	}
 
-	return <div className={styles.new_releases}>{content}</div>
-}
+// 	return <div className={styles.new_releases}>{content}</div>
+// }
 
-export default AlbumsList
+// export default AlbumsList
