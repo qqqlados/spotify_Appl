8ca995543f5bc750459cd4f465ccaa7b427e1d37
wladus <wladusdrem@gmail.com>
2024-08-08 21:42:42 +0300

    smth7

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -8,6 +8,8 @@ const Login = () => {
 		setCookies()
 	}, [])
 
+	console.log(import.meta.env.VITE_REDIRECT_URL + '/login')
+
 	return (
 		<motion.div className={styles.container} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
 			<div className={styles.div}>
